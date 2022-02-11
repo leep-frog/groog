9a392b8039f96c2bbf3b43a3fc06fa8660993307
leep-frog <gleeper49@gmail.com>
2022-02-10 19:55:45 -0500

    make web app

--- bundle-extension.ps1
+++
@@ -1,7 +0,0 @@
-Push-Location .
-Set-Location C:\Users\gleep\OneDrive\Desktop\Coding\vs-code\groog
-vsce package 
-Pop-Location
-code --install-extension C:\Users\gleep\OneDrive\Desktop\Coding\vs-code\groog\groog-0.0.1.vsix
-Write-Host "Installation complete!"
-Start-Sleep 2
