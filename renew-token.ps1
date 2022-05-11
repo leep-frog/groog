691b3ea55cd634a04d0075fbf17f29d5ddcdea67
leep-frog <gleeper49@gmail.com>
2022-05-11 09:05:33 -0400

    renew token

--- renew-token.ps1
+++ renew-token.ps1
@@ -1,4 +1,8 @@
-Write-Output Create an access token by going to
-Write-Output https://dev.azure.com/groogle/_usersSettings/tokens
-Write-Output Paste the token (right click) for the below prompt
+Write-Output "====================================================="
+Write-Output "Create an access token by going to"
+Write-Output "https://dev.azure.com/groogle/_usersSettings/tokens"
+Write-Output "Enter (y) to overwrite PAT and then paste"
+Write-Output "the token (right click) in the next prompt"
+Write-Output "====================================================="
 vsce login groogle
+Start-Sleep 10
