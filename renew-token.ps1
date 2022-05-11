677cf269687cab7e226927a8ff631136a80c48a4
leep-frog <gleeper49@gmail.com>
2022-05-11 09:02:28 -0400

    publish v0.0.3

---
+++ renew-token.ps1
@@ -0,0 +1,4 @@
+Write-Output Create an access token by going to
+Write-Output https://dev.azure.com/groogle/_usersSettings/tokens
+Write-Output Paste the token (right click) for the below prompt
+vsce login groogle
