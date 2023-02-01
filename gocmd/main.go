541083c267af387c76a0c79f1bb261535c06c7cd
leep-frog <gleeper49@gmail.com>
2023-02-01 16:16:22 -0500

    update version

--- gocmd/main.go
+++ gocmd/main.go
@@ -17,7 +17,7 @@ func node() *command.Node {
 			Name:        "groog",
 			DisplayName: "groog",
 			Description: "",
-			Version:     "0.0.99",
+			Version:     "0.0.100",
 			Publisher:   "groogle",
 			Browser:     "./dist/web/extension.js",
 			Engines: map[string]string{
