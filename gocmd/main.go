46d00dfb0e2ae4e4ae9f468de156e875d8c12cbf
Greg Leeper <gleeper49@gmail.com>
2023-09-19 12:57:25 -0400

    regenerate package.json after updating version number

--- gocmd/main.go
+++ gocmd/main.go
@@ -33,89 +33,87 @@ func (*cli) Changed() bool   { return false }
 
 var (
 	versionRegex = regexp.MustCompile("^(\\s*Version:\\s*[\"`])([0-9\\.]+)([\"`],)$")
+	runtimeNode  = command.RuntimeCaller()
 )
 
 func (c *cli) Node() command.Node {
 	versionSectionArg := command.OptionalArg[int]("VERSION", "Version section offset (0 for smallest, 1 for middle, 2 for major)", command.Default(0), command.Between(0, 2, true))
 
-	runtimeNode := command.RuntimeCaller()
-
-	return &command.BranchNode{
-		Branches: map[string]command.Node{
-			"update u": command.SerialNodes(
-				versionSectionArg,
-				&command.ExecutorProcessor{func(o command.Output, d *command.Data) error {
-					_, fileName, _, ok := runtime.Caller(0)
-					if !ok {
-						return o.Stderrf("failed to get runtime.Caller")
-					}
-
-					// Go two directories up (to groog root)
-					packageFile := filepath.Join(filepath.Dir(fileName), "package.go")
-
-					b, err := os.ReadFile(packageFile)
-					if err != nil {
-						return o.Annotatef(err, "failed to read package.go")
-					}
-
-					contents := strings.Split(string(b), "\n")
-					var newContents []string
-					var replaced int
-					var newVersion string
-					for _, line := range contents {
-						m := versionRegex.FindStringSubmatch(line)
-						if len(m) > 0 {
-							replaced++
-							prefix, version, suffix := m[1], m[2], m[3]
-							versionParts := strings.Split(version, ".")
-
-							indexToChange := len(versionParts) - 1 - versionSectionArg.Get(d)
-
-							vNum, err := strconv.Atoi(versionParts[indexToChange])
-							if err != nil {
-								return o.Annotatef(err, "failed to convert version")
-							}
-
-							// Clear out smaller versions
-							for i := indexToChange; i < len(versionParts); i++ {
-								versionParts[i] = "0"
-							}
-							versionParts[indexToChange] = fmt.Sprintf("%d", vNum+1)
-							newVersion = strings.Join(versionParts, ".")
-							line = fmt.Sprintf("%s%s%s", prefix, newVersion, suffix)
+	return command.SerialNodes(
+		runtimeNode,
+		&command.BranchNode{
+			Branches: map[string]command.Node{
+				"update u": command.SerialNodes(
+					versionSectionArg,
+					&command.ExecutorProcessor{func(o command.Output, d *command.Data) error {
+						_, fileName, _, ok := runtime.Caller(0)
+						if !ok {
+							return o.Stderrf("failed to get runtime.Caller")
 						}
-						newContents = append(newContents, line)
-					}
 
-					if replaced == 0 {
-						return o.Stderrf("Made no replacements")
-					}
+						// Go two directories up (to groog root)
+						packageFile := filepath.Join(filepath.Dir(fileName), "package.go")
 
-					if err := os.WriteFile(packageFile, []byte(strings.Join(newContents, "\n")), 0644); err != nil {
-						return o.Annotatef(err, "failed to write new contents to package.go")
-					}
+						b, err := os.ReadFile(packageFile)
+						if err != nil {
+							return o.Annotatef(err, "failed to read package.go")
+						}
 
-					o.Stdoutln("Successfully updated to new version:", newVersion)
-					return nil
+						contents := strings.Split(string(b), "\n")
+						var newContents []string
+						var replaced int
+						var newVersion string
+						for _, line := range contents {
+							m := versionRegex.FindStringSubmatch(line)
+							if len(m) > 0 {
+								replaced++
+								prefix, version, suffix := m[1], m[2], m[3]
+								versionParts := strings.Split(version, ".")
+
+								indexToChange := len(versionParts) - 1 - versionSectionArg.Get(d)
+
+								vNum, err := strconv.Atoi(versionParts[indexToChange])
+								if err != nil {
+									return o.Annotatef(err, "failed to convert version")
+								}
+
+								// Clear out smaller versions
+								for i := indexToChange; i < len(versionParts); i++ {
+									versionParts[i] = "0"
+								}
+								versionParts[indexToChange] = fmt.Sprintf("%d", vNum+1)
+								newVersion = strings.Join(versionParts, ".")
+								line = fmt.Sprintf("%s%s%s", prefix, newVersion, suffix)
+							}
+							newContents = append(newContents, line)
+						}
+
+						if replaced == 0 {
+							return o.Stderrf("Made no replacements")
+						}
+
+						if err := os.WriteFile(packageFile, []byte(strings.Join(newContents, "\n")), 0644); err != nil {
+							return o.Annotatef(err, "failed to write new contents to package.go")
+						}
+
+						o.Stdoutln("Successfully updated to new version:", newVersion)
+
+						return c.regeneratePackageJson(o, d)
+					}},
+				),
+			},
+			Default: command.SerialNodes(
+				&command.ExecutorProcessor{func(o command.Output, d *command.Data) error {
+					return c.regeneratePackageJson(o, d)
 				}},
 			),
 		},
-		Default: command.SerialNodes(
-			runtimeNode,
-			&command.ExecutorProcessor{func(o command.Output, d *command.Data) error {
-				path := filepath.Join(filepath.Dir(filepath.Dir(runtimeNode.Get(d))), "package.json")
-				if err := c.execute(path); err != nil {
-					return err
-				}
-
-				o.Stdoutln("Successfully updated package.json")
-				return nil
-			}},
-		),
-	}
+	)
 }
 
-func (c *cli) execute(filename string) error {
+func (c *cli) regeneratePackageJson(o command.Output, d *command.Data) error {
+	filename := filepath.Join(filepath.Dir(filepath.Dir(runtimeNode.Get(d))), "package.json")
+
 	p := groogPackage()
 
 	j, err := json.MarshalIndent(p, "", "  ")
@@ -127,5 +125,6 @@ func (c *cli) execute(filename string) error {
 		return fmt.Errorf("failed to write json to output file: %v", err)
 	}
 
+	o.Stdoutln("Successfully updated package.json")
 	return nil
 }
