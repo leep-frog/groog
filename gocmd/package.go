7b8d051a85c5b79558e657a4134dcf86b5162a45
Greg Leeper <gleeper49@gmail.com>
2023-11-02 19:12:09 -0400

    update package

--- gocmd/package.go
+++ gocmd/package.go
@@ -7,7 +7,7 @@ func groogPackage(versionOverride string) *Package {
 		Name:        "groog",
 		DisplayName: "groog",
 		Description: "",
-		Version:     "1.0.47",
+		Version:     "1.0.48",
 		Publisher:   "groogle",
 		Main:        "./out/extension.js",
 		Engines: map[string]string{
