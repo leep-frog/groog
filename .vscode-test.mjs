dda6b030eecf9dc36c74e30252aac89187c1bcbb
Greg Leeper <gleeper49@gmail.com>
2024-03-05 15:20:39 -0500

    first pass at mjs config

--- .vscode-test.mjs
+++ .vscode-test.mjs
@@ -1,5 +1,16 @@
 import { defineConfig } from '@vscode/test-cli';
+import path from 'path';
 
 export default defineConfig({
 	files: 'out/test/**/*.test.js',
+  // download: {
+    // timeout: 123,
+  // },
+  // dow
+  useInstallation: {
+    fromMachine: true,
+    fromPath: path.resolve(".vscode-test", "vscode-win32-x64-archive-1.87.0", "Code.exe"),
+    // fromPath: path.resolve(__dirname, ".//"),
+  },
+
 });
