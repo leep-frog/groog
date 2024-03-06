553df383f1e48afc5fb0aa4c2249f7ec91dfc62e
Greg Leeper <gleeper49@gmail.com>
2024-03-05 20:02:36 -0500

    test coverage report

--- .vscode-test.mjs
+++ .vscode-test.mjs
@@ -3,14 +3,10 @@ import path from 'path';
 
 export default defineConfig({
 	files: 'out/test/**/*.test.js',
-  // download: {
-    // timeout: 123,
-  // },
-  // dow
   useInstallation: {
     fromMachine: true,
-    fromPath: path.resolve(".vscode-test", "vscode-win32-x64-archive-1.87.0", "Code.exe"),
-    // fromPath: path.resolve(__dirname, ".//"),
+    // This needs to be a different version than the one I'm actually using, otherwise test doesn't run
+    fromPath: path.resolve(".vscode-test", "vscode-win32-x64-archive-1.86.0", "Code.exe"),
   },
 
 });
