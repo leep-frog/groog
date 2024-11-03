803ab9e19fb62f47f1f41ce126586037124c0353
Greg Leeper <gleeper49@gmail.com>
2024-11-03 15:18:23 -0500

    fix vscodeignore

--- gocmd/package.go
+++ gocmd/package.go
@@ -7,7 +7,7 @@ func groogPackage(versionOverride string) *Package {
 		Name:        "groog",
 		DisplayName: "groog",
 		Description: "",
-		Version:     "2.7.23",
+		Version:     "2.7.24",
 		Publisher:   "groogle",
 		Main:        "./out/extension.js",
 		Engines: map[string]string{
